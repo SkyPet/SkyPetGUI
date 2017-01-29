50d225900ab90e42e8b9488348fd47cb59576447
Daniel Stahl <danstahl1138@gmail.com>
2017-01-28 18:36:57 -0600

    made testserver correspond with the skypet wrapper for geth

--- src/SkyPetProgress.js
+++ src/SkyPetProgress.js
@@ -3,6 +3,6 @@ import CircularProgress from 'material-ui/CircularProgress';
 export const MyProgressBar=({value, centerComponent})=>{
   return value>0?<div style={centerComponent}><CircularProgress  key="firstCircle" size={80} thickness={5} mode="determinate"  value={value}/></div>:<div style={centerComponent}><CircularProgress  key="secondCircle" size={80} thickness={5} /></div>
 }
-export const SyncWrap=({isSyncing, children, progress})=>{
-  return isSyncing?<MyProgressBar value={progress}/>:children
+export const SyncWrap=({isSyncing, children, progress, centerComponent})=>{
+  return isSyncing?<MyProgressBar centerComponent={centerComponent} value={progress}/>:children
 }
--- testWSServer.js
+++ testWSServer.js
@@ -24,23 +24,30 @@ wss.on('connection', function(ws) {
                 progress+=10;
                 if(progress>100){
                     clearInterval(doProgress);
-                    return  ws.send(makeObj('sync', {currentProgress:100, isSyncing:false}));
+                    
+                    ws.send(makeObj('account', "MyTestAccount"));
+                    ws.send(makeObj('moneyInAccount', 100));
+                    ws.send(makeObj('cost',1));
+                    ws.send(makeObj('sync', {currentProgress:100, isSyncing:false}));
+                    
                 }
                 ws.send(makeObj('sync', {currentProgress:progress, isSyncing:true}));
+                
             }, 1000);
         }
-        else if(val.password){
-            if(val.password!==testPassword){
-                return ws.send(makeObj('passwordError', "Wrong password"));
+        else if(val.id){
+            ws.send(makeObj('retrievedData', [{timestamp:new Date().toISOString(), value:"Hello World", isEncrypted:false, attributeType:"0"}]))
+        }
+        else if(val.addAttribute){
+            if(val.addAttribute.password===testPassword){
+                ws.send(makeObj('attributeAdded', true));
             }
-            const Ids=getIds();
-            ws.send(makeObj('successLogin', 'MyTestAccount'))
-            ws.send(makeObj('petId', Ids.hashId));
-            ws.send(makeObj('account', "MyTestAccount"));
-            ws.send(makeObj('constractAddress', "MyTestContractAddress"));    
+            else{
+                ws.send(makeObj('passwordError', "Incorrect Password")); 
+            }
+        }
+        else if(val.getAttributes){
             ws.send(makeObj('retrievedData', [{timestamp:new Date(), attributeText:"Hello World", isEncrypted:false, attributeType:"0"}]))
-            ws.send(makeObj('moneyInAccount',1000));
-            ws.send(makeObj('cost',1));
         }
         
     })
